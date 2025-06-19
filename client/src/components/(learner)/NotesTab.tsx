bff01be19c4a0079383b2c858962e784465b1515
dlord213 <lollingworld41@gmail.com>
2025-06-19 21:06:16 +0800

    client: added global modals for quick study tools in the sidebar

--- client/src/components/(learner)/NotesTab.tsx
+++ client/src/components/(learner)/NotesTab.tsx
@@ -11,32 +11,33 @@ export default function NotesTab({ notes }: { notes: any[] }) {
           <Search size={24} />
           <input type="search" className="lg:w-md" placeholder="Search" />
         </label>
-        <NavLink
-          to="/learner/note/create"
-          className="btn btn-soft btn-success"
-        >
+        <NavLink to="/learner/note/create" className="btn btn-soft btn-success">
           <Plus />
           <h1>Manually create</h1>
         </NavLink>
       </div>
+      <div className="filter">
+        <input
+          className="btn filter-reset"
+          type="radio"
+          name="metaframeworks"
+          aria-label="All"
+        />
+        <input
+          className="btn"
+          type="radio"
+          name="metaframeworks"
+          aria-label="From user"
+        />
+        <input
+          className="btn"
+          type="radio"
+          name="metaframeworks"
+          aria-label="AI-Generated"
+        />
+      </div>
       <div className="flex flex-row gap-4 flex-wrap">
         <NoteCard link="1" />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
       </div>
     </>
   );
--- client/src/routes/(learner)/LearnerLayout.tsx
+++ client/src/routes/(learner)/LearnerLayout.tsx
@@ -1,36 +1,29 @@
+import GenerateFlashcardModal from "@/components/(ai)/GenerateFlashcardModal_GLOBAL";
+import GenerateQuizModal from "@/components/(ai)/GenerateQuizModal_GLOBAL";
+import GenerateSummaryModal from "@/components/(ai)/GenerateSummaryModal_GLOBAL";
 import Sidebar from "@/components/(learner)/Sidebar";
 import TimerPopup from "@/components/(learner)/TimerPopup";
 import useTimer from "@/hooks/useTimer";
-import startSound from "/assets/audio/start.wav";
-import endSound from "/assets/audio/end.wav";
 
 import { Outlet, useLoaderData, useLocation } from "react-router";
 import { Toaster } from "sonner";
-import { useRef } from "react";
 
 export default function LearnerLayout() {
   const data = useLoaderData();
   const { pathname } = useLocation();
   const { isPopupEnabled } = useTimer();
 
-  const startAudioRef = useRef(null);
-  const endAudioRef = useRef(null);
-
   return (
     <>
-      <audio
-        ref={startAudioRef}
-        src={startSound}
-        preload="auto"
-        autoPlay={false}
-      />
-      <audio ref={endAudioRef} src={endSound} preload="auto" autoPlay={false} />
       <Toaster position="top-right" />
       <main className="flex flex-col lg:flex-row min-h-screen bg-base-200">
         <Sidebar tab={pathname} />
         <Outlet />
       </main>
       {isPopupEnabled ? <TimerPopup /> : <></>}
+      <GenerateSummaryModal />
+      <GenerateFlashcardModal />
+      <GenerateQuizModal />
     </>
   );
 }
