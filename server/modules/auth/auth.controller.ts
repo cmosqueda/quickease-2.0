fd59776ca5081d3a4ff329451cc5681b8ed3deb2
tyne <mosqueda.christinereisa04@gmail.com>
2025-06-28 17:48:47 +0800

    update: synced all server modules changes

--- server/modules/auth/auth.controller.ts
+++ server/modules/auth/auth.controller.ts
@@ -4,11 +4,11 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import { loginUser, registerUser } from "./auth.service";
 
 export async function login_user(request: FastifyRequest, reply: FastifyReply) {
-  try {
-    const { email, password } = request.body as {
-      email: string;
-      password: string;
-    };
+    try {
+        const { email, password } = request.body as {
+            email: string;
+            password: string;
+        };
 
         if (!email || !password) {
             return reply.code(400).send({ message: "Email and password are required." });
@@ -36,41 +36,16 @@ export async function login_user(request: FastifyRequest, reply: FastifyReply) {
             message: "Internal server error. Please try again later."
         });
     }
-
-    const user = await loginUser(email, password);
-
-    if (!user) {
-      return reply.code(400).send({ message: "Check your credentials." });
-    }
-
-    const token = await reply.jwtSign(user);
-
-    reply
-      .setCookie("QUICKEASE_TOKEN", token, {
-        path: "/",
-        secure: true,
-        httpOnly: true,
-        sameSite: "strict",
-      })
-      .code(200)
-      .send({
-        is_admin: user.is_admin,
-      });
-  } catch (err) {
-    reply.code(500).send({
-      message: "Internal server error. Please try again later.",
-    });
-  }
 }
 
 export async function register_user(request: FastifyRequest, reply: FastifyReply) {
-  try {
-    const { firstName, lastName, email, password } = request.body as {
-      firstName: string;
-      lastName: string;
-      email: string;
-      password: string;
-    };
+    try {
+        const { firstName, lastName, email, password } = request.body as {
+            firstName: string;
+            lastName: string;
+            email: string;
+            password: string;
+        };
 
         if (!firstName) {
             return reply.code(400).send({ message: "Error: First name required." })
@@ -113,42 +88,13 @@ export async function register_user(request: FastifyRequest, reply: FastifyReply
             message: "Internal server error. Could not register user."
         });
     }
-
-    const existed = await db_client.user.findUnique({
-      where: { email },
-    });
-
-    if (existed) {
-      return reply.code(406).send({ message: "Email already in use." });
-    }
-
-    const user = await registerUser(firstName, lastName, email, password);
-    const token = await reply.jwtSign(user);
-
-    reply
-      .setCookie("QUICKEASE_TOKEN", token, {
-        path: "/",
-        httpOnly: true,
-        secure: true,
-        sameSite: "strict",
-      })
-      .code(201)
-      .send({
-        role: "user",
-      });
-  } catch (err) {
-    error("Register error:", err);
-    reply.code(500).send({
-      message: "Internal server error. Could not register user.",
-    });
-  }
 }
 
 export async function logout(request: FastifyRequest, reply: FastifyReply) {
-  try {
-    reply.clearCookie("QUICKEASE_TOKEN");
-    return reply.code(200).send("Logout successfully.");
-  } catch (err) {
-    return reply.code(500).send("Error occurred while logging out.");
-  }
+    try {
+        reply.clearCookie('QUICKEASE_TOKEN');
+        return reply.code(200).send('Logout successfully.');
+    } catch (err) {
+        return reply.code(500).send('Error occurred while logging out.');
+    }
 }
